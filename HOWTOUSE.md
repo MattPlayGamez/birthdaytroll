cafce55787a8a59a783721ca27ab18ced432e07d
Mathys Penson <penson.mathys@gmail.com>
2022-06-06 14:52:42 +0200

    first commit

---
+++ HOWTOUSE.md
@@ -0,0 +1,4 @@
+# Just change the name in the script.js
+# const friendsname = '[NAME]'
+
+# You can easily use netlify.app and just drag and drop the folder. Send your friend the link. Enjoy him being rickrolled
--- script.js
+++ script.js
@@ -1,17 +1,19 @@
+//Import the button "hide" element and the iframe
 const btn = document.getElementById('clickme')
 const hide = document.getElementById('hide')
 const iframe = document.getElementById('iframe')
 
-const frame = `<iframe width="1590" height="850" src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="YouTube video player" frameborder="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+//variable for "friendsname"
+const friendsname = 'Dante' // Type your friends name here
 
+
+// Execution code when clicking the button
 btn.addEventListener('click', (e) => {
-    e.preventDefault()
-    const friendsname = 'Dante'
-    hide.classList.add('hidden')
-    btn.classList.add('hidden')
-    alert('Happy Birthday ' + friendsname)
-    iframe.classList.remove('visually-hidden')
-    iframe.requestFullscreen()
-    iframe.src = 'https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1'
-    fetch('https://accountsgoogle.tk')
+    e.preventDefault() // preventing it from reloading
+    hide.classList.add('hidden') // adding the "hidden" tag to the "hide" id
+    btn.classList.add('hidden') // adding the "hidden" tag to the button
+    alert('Happy Birthday ' + friendsname) // showing a message
+    iframe.classList.remove('visually-hidden') // removing the "visually-hidden" class from the iframe
+    iframe.requestFullscreen() // setting the iframe to fullscreen
+    iframe.src = 'https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1' // new source url with autoplay enabled (autoplay=1)
 })
